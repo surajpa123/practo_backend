fdc04cc29f8c9b18757bf0f9efbd8dd5b2934a19
surajpa123 <77233053+surajpa123@users.noreply.github.com>
2022-04-02 12:21:14 +0530

    added dicription page

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const express= require("express");
 const app = express()
 const data = require("./db.json")
 let PORT = process.env.port || 8080
+
+
+const host = '0.0.0.0';
 app.get("/", (req,res)=>{
     res.send("Hello");
 })
 app.get("/medicine", (req,res)=>{
     res.send(data);
 })
-app.listen(PORT, async () => {
+app.listen(PORT,host, async () => {
     try {
         console.log(`listening on port ${PORT}`);
     } catch (err) {
