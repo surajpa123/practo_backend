039c76505f4c0a703449431b42a40396e38cf75b
surajpa123 <77233053+surajpa123@users.noreply.github.com>
2022-04-02 12:12:29 +0530

    added dicription page

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ require('dotenv').config()
 const express= require("express");
 const app = express()
 const data = require("./db.json")
-let port = process.env.port || 8080
+let PORT = process.env.port || 8080
 app.get("/", (req,res)=>{
     res.send("Hello");
 })
 app.get("/medicine", (req,res)=>{
     res.send(data);
 })
-app.listen(port,()=>{
-    
-    console.log(`Listening on port ${port}`)
-})
+app.listen(PORT, async () => {
+    try {
+        console.log(`listening on port ${PORT}`);
+    } catch (err) {
+      console.error(err.message);
+    }
+  
+  });
 
 
 
